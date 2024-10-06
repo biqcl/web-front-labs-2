d09ba839bf80e3d44a6a1eb3e3d8417dff9b7f62
Мария Бызова <m_byzovaa@mail.ru>
2024-10-06 14:29:00 +0700

    Cookie

--- lab3.py
+++ lab3.py
@@ -1,8 +1,27 @@
-from flask import  render_template, Blueprint
+from flask import  render_template, Blueprint, request, make_response, redirect
 lab3 = Blueprint('lab3', __name__)
 
 
 @lab3.route('/lab3/')
 def lab():
-    return render_template('lab3.html')
+    name = request.cookies.get('name')
+    name_color = request.cookies.get('name_color')
+    return render_template('lab3/lab3.html', name=name, name_color=name_color)
 
+
+@lab3.route('/lab3/cookie/')
+def cookie():
+    resp = make_response(redirect('/lab3/'))
+    resp.set_cookie('name', 'Mar', max_age=5)
+    resp.set_cookie('age', '20')
+    resp.set_cookie('name_color', '#A3CCA3')
+    return resp
+
+
+@lab3.route('/lab3/del_cookie/')
+def del_cookie():
+    resp = make_response(redirect('/lab3/'))
+    resp.delete_cookie('name')
+    resp.delete_cookie('age')
+    resp.delete_cookie('name_color')
+    return resp
