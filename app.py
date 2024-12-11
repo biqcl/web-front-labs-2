0aa32d92af721a1d181a6c1ae803e53a31021b82
Мария Бызова <m_byzovaa@mail.ru>
2024-12-11 16:53:49 +0700

    edited

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from lab4 import lab4
 from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
+from lab8 import lab8
 
 
 app = Flask(__name__)
@@ -22,6 +23,7 @@ app.register_blueprint(lab4)
 app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
+app.register_blueprint(lab8)
 
 
 @app.route("/")
@@ -48,6 +50,7 @@ def index():
         <br><a href="/lab5">&#10023; Пятая лабораторная</a>
         <br><a href="/lab6">&#10023; Шестая лабораторная</a>
         <br><a href="/lab7">&#10023; Седьмая лабораторная</a>
+        <br><a href="/lab8">&#10023; Восьмая лабораторная</a>
 
         <footer>
             &copy; Бызова Мария, ФБИ-22, 3 курс, 2024
