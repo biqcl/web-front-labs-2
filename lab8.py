df78957eb3ef37cb3fa3b78ed6def8b94e0b1371
Мария Бызова <m_byzovaa@mail.ru>
2024-12-18 23:38:33 +0700

    Регистрация

--- lab8.py
+++ lab8.py
@@ -2,8 +2,11 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 import sqlite3
 from os import path
-from flask import  render_template, Blueprint, request, jsonify, current_app
-from datetime import datetime
+from flask import  render_template, Blueprint, request, jsonify, current_app, redirect
+from db import db
+from db.models import users, articles
+from werkzeug.security import check_password_hash, generate_password_hash
+
 
 lab8 = Blueprint('lab8', __name__)
 
@@ -32,9 +35,38 @@ def db_close(conn, cur):
     conn.close()
 
 
-@lab8.route('/la8/')
+@lab8.route('/lab8/')
 def lab():
     return render_template('lab8/lab8.html')
 
+
+@lab8.route('/lab8/register/', methods = ['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('lab8/register.html')
+    
+    login_form = request.form.get('login')
+    password_form = request.form.get('password')
+
+    login_error = ""
+    password_error = ""
+
+    if login_form == "":
+        login_error = "Введите логин!"
+    if password_form =="":
+        password_error = "Введите пароль!"
+
+    if login_error or password_error:
+        return render_template('lab8/register.html', login_error=login_error, password_error=password_error)
+    
+    login_exists = users.query.filter_by(login = login_form).first()
+    if login_exists:
+        return render_template('lab8/register.html', error='Такой пользователь уже существует')
+
+    password_hash = generate_password_hash(password_form)
+    new_user = users(login = login_form, password = password_hash)
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect('/lab8/') 
 
 
