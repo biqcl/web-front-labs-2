85cc6ed3d1bfb01cc0bebd6113ad2b5109439dcb
Мария Бызова <m_byzovaa@mail.ru>
2024-12-19 12:09:49 +0700

    Авторизация

--- lab8.py
+++ lab8.py
@@ -2,10 +2,13 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 import sqlite3
 from os import path
-from flask import  render_template, Blueprint, request, jsonify, current_app, redirect
+from flask import  render_template, Blueprint, request, current_app, redirect, session
 from db import db
 from db.models import users, articles
 from werkzeug.security import check_password_hash, generate_password_hash
+from flask_login import login_user, login_required, current_user
+
+
 
 
 lab8 = Blueprint('lab8', __name__)
@@ -69,4 +72,38 @@ def register():
     db.session.commit()
     return redirect('/lab8/') 
 
+
+@lab8.route('/lab8/login/', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('/lab8/login.html')
+    
+    login_form = request.form.get('login')
+    password_form = request.form.get('password')
+
+    login_error = ""
+    password_error = ""
+
+    if login_form == "":
+        login_error = "Введите логин!"
+    if password_form =="":
+        password_error = "Введите пароль!"
+
+    if login_error or password_error:
+        return render_template('lab8/register.html', login_error=login_error, password_error=password_error)
+        
+    user = users.query.filter_by(login = login_form).first()
+
+    if user:
+        if check_password_hash(user.password, password_form):
+            login_user(user, remember = False)
+            return redirect('/lab8/')        
+   
+    return render_template('/lab8/login.html', error = 'Ошибка входа: логин и/или пароль неверны')
+
+
+@lab8.route('/lab8/articles/')
+@login_required
+def article_list():
+    return "Список статей"
 
